8b221acaa9c0a95018ad8444f3085c67d00949ba
Gleb Makarenko <113942267+Glibusss@users.noreply.github.com>
2025-10-05 15:53:17 +0300

    CssVars class (#5)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 export * as Color from "./Color";
 export * as CSS from "./CSS";
+export * as CssVars from "./CssVars";
 
 import * as Color from "./Color";
 import * as CSS from "./CSS";
+import * as CssVars from "./CssVars";
 
 export default {
   ...Color,
   ...CSS,
+  ...CssVars,
 };
